4caa2bd72197251a285d860b734ee66133119c65
AlexKosterny <alexkosterny@gmail.com>
2015-12-30 11:14:18 +0200

    add VectorField class

--- VectorFieldTest/VectorField.cs
+++ VectorFieldTest/VectorField.cs
@@ -1,13 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace VectorFieldTest
 {
     class VectorField
     {
-        //private List<vec2> dsgf;
+        public VectorField(float nx, float ny, float dx, float dy, int n, int m)
+        {
+            N = n;
+            M = m;
+            mCell = new PointCell[n][];
+            for (int i=0; i<N; ++i)
+            {
+                mCell[i] = new PointCell[m];
+                for (int j = 0; j < M; ++j)
+                {
+                    mCell[i][j] = new PointCell(nx+j*dx, ny+i*dy);
+                    if (i != 0)
+                    {
+                        mCell[i][j].setNeighbors(2, mCell[i - 1][j]);
+                    }
+                    if (j != 0)
+                    {
+                        mCell[i][j].setNeighbors(3, mCell[i][j - 1]);
+                    }
+                }
+            }
+        }
+        public void applyRelax()
+        {
+            for (int i = 0; i < N; ++i)
+            {
+                for (int j = 0; j < M; ++j)
+                {
+                    mCell[i][j].applyRelax();
+                }
+            }
+        }
+        public void draw(Graphics g)
+        {
+            for (int i = 0; i < N; ++i)
+            {
+                for (int j = 0; j < M; ++j)
+                {
+                    mCell[i][j].draw(g);
+                }
+            }
+        }
+        public void applyForce(int x, int y, vec2 force)
+        {
+            if (x >= 0 && x < M && y >= 0 && y < N)
+            {
+                mCell[y][x].applyForce(force);
+            }
+        }
+        private PointCell[][] mCell;
+        private int N, M;
     }
 }
